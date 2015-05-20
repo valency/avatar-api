5c12ba41cc28b94e50c73279acaa5b1625794875
Ye Ding <guiewy@gmail.com>
2015-05-20 14:21:05 +0800

    init

--- api/settings.py
+++ api/settings.py
@@ -61,7 +61,7 @@ DATABASES = {
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC+8'
+TIME_ZONE = 'Asia/Hong_Kong'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
