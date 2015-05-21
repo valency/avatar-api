7d99e38567a470300b3e2405168e0451c851cfc3
Ye Ding <guiewy@gmail.com>
2015-05-21 15:40:45 +0800

    v0.5.21

--- avatar/urls.py
+++ avatar/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from rest_framework import routers
 
 import views
@@ -7,6 +7,8 @@ router = routers.DefaultRouter()
 router.register(r'trajectories', views.TrajectoryViewSet)
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^api/traj/add', views.create_traj, name='api/traj/add'),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    # url(r'^$', views.index, name='index'),
+    # url(r'^api/traj/add', views.create_traj, name='api/traj/add'),
 ]
