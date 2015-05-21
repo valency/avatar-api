fcc846dba41b78399e25d0f6d30d3abda44c7ec4
Ye Ding <guiewy@gmail.com>
2015-05-21 15:49:28 +0800

    v0.5.21

--- avatar/serializers.py
+++ avatar/serializers.py
@@ -3,49 +3,49 @@ from rest_framework import serializers
 from models import *
 
 
-class PointSerializer(serializers.Serializer):
+class PointSerializer(serializers.ModelSerializer):
     class Meta:
         model = Point
         fields = ('lat', 'lng')
 
 
-class SampleSerializer(serializers.Serializer):
+class SampleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sample
         fields = ('id', 'p', 't', 'speed', 'angle', 'occupy', 'meta', 'src')
 
 
-class IntersectionSerializer(serializers.Serializer):
+class IntersectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Intersection
         fields = ('id', 'p')
 
 
-class RoadSerializer(serializers.Serializer):
+class RoadSerializer(serializers.ModelSerializer):
     class Meta:
         model = Road
         fields = ('id', 'name', 'type', 'length', 'speed', 'p', 'intersection')
 
 
-class TraceSerializer(serializers.Serializer):
+class TraceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Trace
         fields = ('id', 'p')
 
 
-class PathFragmentSerializer(serializers.Serializer):
+class PathFragmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = PathFragment
         fields = ('road', 'p')
 
 
-class PathSerializer(serializers.Serializer):
+class PathSerializer(serializers.ModelSerializer):
     class Meta:
         model = Path
         fields = ('id', 'road')
 
 
-class TrajectorySerializer(serializers.Serializer):
+class TrajectorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Trajectory
         fields = ('id', 'taxi', 'trace', 'path')
