276f6043d16fff58c5273342b47716b77aea2e2a
Ye Ding <guiewy@gmail.com>
2015-05-25 03:45:07 +0800

    v0.5.24

--- avatar/serializers.py
+++ avatar/serializers.py
@@ -16,36 +16,50 @@ class SampleMetaSerializer(serializers.ModelSerializer):
 
 
 class SampleSerializer(serializers.ModelSerializer):
+    p = PointSerializer()
+    meta = SampleMetaSerializer(many=True)
+
     class Meta:
         model = Sample
         fields = ('id', 'p', 't', 'speed', 'angle', 'occupy', 'meta', 'src')
 
 
 class IntersectionSerializer(serializers.ModelSerializer):
+    p = PointSerializer()
+
     class Meta:
         model = Intersection
         fields = ('id', 'p')
 
 
 class RoadSerializer(serializers.ModelSerializer):
+    p = PointSerializer(many=True)
+    intersection = IntersectionSerializer(many=True)
+
     class Meta:
         model = Road
         fields = ('id', 'name', 'type', 'length', 'speed', 'p', 'intersection')
 
 
 class TraceSerializer(serializers.ModelSerializer):
+    p = SampleSerializer(many=True)
+
     class Meta:
         model = Trace
         fields = ('id', 'p')
 
 
 class PathFragmentSerializer(serializers.ModelSerializer):
+    road = RoadSerializer()
+
     class Meta:
         model = PathFragment
         fields = ('road', 'p')
 
 
 class PathSerializer(serializers.ModelSerializer):
+    road = PathFragmentSerializer(many=True)
+
     class Meta:
         model = Path
         fields = ('id', 'road')
