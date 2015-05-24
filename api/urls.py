900800c3f8c8e49bf41309dff7befe8c5d42282b
Ye Ding <guiewy@gmail.com>
2015-05-25 02:09:10 +0800

    v0.5.24

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^polls/', include('polls.urls')),
     url(r'^avatar/', include('avatar.urls'))
 ]
