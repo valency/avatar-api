f98aa867d2128bab1be679aa9d2ef6cf662f047f
Ye Ding <guiewy@gmail.com>
2015-09-04 16:27:35 +0800

    v0.9.4

--- api/settings.py
+++ api/settings.py
@@ -79,7 +79,7 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Asia/Hong_Kong'
 USE_I18N = True
 USE_L10N = True
-USE_TZ = True
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
