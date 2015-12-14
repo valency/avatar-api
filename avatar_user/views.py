e97eeaa0c269e370f3ad7e3b3f21caed71a37785
Ye Ding <guiewy@gmail.com>
2015-12-14 22:44:32 +0800

    v0.12.14

--- avatar_user/views.py
+++ avatar_user/views.py
@@ -22,10 +22,10 @@ def login_or_register(request):
             Account.objects.get(username=request.POST["username"])
         except ObjectDoesNotExist:
             print "Not found, perform registration..."
-            register(request)
+            perform_register(request.POST["username"], request.POST["password"])
             print "Successfully registered."
         print "Logging in..."
-        return login(request)
+        return perform_login(request.POST["username"], request.POST["password"])
     else:
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
@@ -34,34 +34,42 @@ def login_or_register(request):
 def register(request):
     print "Received registration request."
     if "username" in request.POST and "password" in request.POST:
-        try:
-            Account.objects.get(username=request.POST["username"])
-            return Response(status=status.HTTP_409_CONFLICT)
-        except ObjectDoesNotExist:
-            account = Account(username=request.POST["username"], password=request.POST["password"], register_time=datetime.now())
-            account.save()
-            return Response({"id": account.id})
+        return perform_register(request.POST["username"], request.POST["password"])
     else:
         return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+def perform_register(username, password):
+    try:
+        Account.objects.get(username=username)
+        return Response(status=status.HTTP_409_CONFLICT)
+    except ObjectDoesNotExist:
+        account = Account(username=username, password=password, register_time=datetime.now())
+        account.save()
+        return Response({"id": account.id})
 
 
 @api_view(['POST'])
 def login(request):
     print "Received login request."
     if "username" in request.POST and "password" in request.POST:
-        try:
-            account = Account.objects.get(username=request.POST["username"], password=request.POST["password"])
-            account.ticket = str(uuid.uuid4())
-            account.last_login = datetime.now()
-            account.save()
-            return Response({
-                "id": account.id,
-                "ticket": account.ticket
-            })
-        except ObjectDoesNotExist:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        return perform_login(request.POST["username"], request.POST["password"])
     else:
         return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+def perform_login(username, password):
+    try:
+        account = Account.objects.get(username=username, password=password)
+        account.ticket = str(uuid.uuid4())
+        account.last_login = datetime.now()
+        account.save()
+        return Response({
+            "id": account.id,
+            "ticket": account.ticket
+        })
+    except ObjectDoesNotExist:
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
 
 
 @api_view(['POST'])
