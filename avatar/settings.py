d7f895401ed9a4d48a0bc883ea08292bd709a11a
Ye Ding <guiewy@gmail.com>
2015-12-15 17:15:34 +0800

    v0.12.15

--- avatar/settings.py
+++ avatar/settings.py
@@ -113,6 +113,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Avatar configurations
